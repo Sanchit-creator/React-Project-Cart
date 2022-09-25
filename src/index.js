f057d2fec34e87cf348093aac46c7e70923b41cb
Sanchit98 <sanchit@novolytics.co>
2022-09-25 18:32:52 +0530

    fetched

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// Step 1 line 4,7
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
-// Step 2 line 11 tp 22
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const firebaseConfig = {
   apiKey: "AIzaSyCPj_N36nswoHbj8VCcV7JoX2e5YL4ENhE",
